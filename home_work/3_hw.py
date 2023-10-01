04a21f8a4b60ae10626c70c7ad0a53bfd6a3b074
Irena_Markova <m.irene@mail.ru>
2023-10-01 11:42:30 +0300

    three commit

---
+++ home_work/3_hw.py
@@ -0,0 +1,15 @@
+def num(a, b):
+    if a == b + 135 or a == b - 135:
+        print('yes')
+    else:
+        print('No')
+
+num(135,0)
+
+
+def num(c,d):
+    if (c>d):
+        print('c')
+    else:
+        print('d')
+num(5, 10)
