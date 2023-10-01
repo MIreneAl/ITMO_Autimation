e6cc0530c77c461104ed2417a362fbcb6adc72f0
Irena_Markova <m.irene@mail.ru>
2023-10-01 06:25:26 +0300

    three commit

--- python_testing/task_8_if.py
+++ python_testing/task_8_if.py
@@ -27,3 +27,24 @@ str_1 = 'test'
 str_2 = 'test1'
 
 task_yes_no(str_1, str_2)
+
+
+num_float = 3.4
+# Попробуйте варианты
+num_float = 0
+num_float = -4.5
+
+if num_float > 0:
+    print('Положительное число')
+elif num_float == 0:
+    print('Ноль')
+else:
+    print('Число отрицательное')
+
+
+permit_print = True
+
+if num > 0 and permit_print:
+    print('num - положительное число')
+elif not permit_print:
+    print('Печать запрещена')
