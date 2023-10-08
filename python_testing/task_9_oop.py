5d5e023d516dd26613705a2bdbff1e660dc08dbb
Irena_Markova <m.irene@mail.ru>
2023-10-08 14:59:07 +0300

    three commit

--- python_testing/task_9_oop.py
+++ python_testing/task_9_oop.py
@@ -3,3 +3,14 @@ class Button:
     def __init__(self, text, link):
         self_text = text
         self_link = link
+
+
+# создаем экземпляры класса
+home = Button('Домой', '/ home')
+catalog_msk = Button('Каталог', '/msk/catalog')
+
+# Получаем доступ к атрибутам
+print(home.text)
+print('Кнопка' + home.text + 'Имеет ссылку' + home.link)
+
+print('\n')
