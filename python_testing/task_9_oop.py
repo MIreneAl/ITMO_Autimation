67c5c48531a698339fcbf7cfe02c3fab53862f26
Irena_Markova <m.irene@mail.ru>
2023-10-10 16:58:36 +0300

    three commit

--- python_testing/task_9_oop.py
+++ python_testing/task_9_oop.py
@@ -20,3 +20,20 @@ print('Кнопка' + catalog_msk.text + 'Имеет ссылку' + catalog_ms
 
 
 class Button_Two:
+
+ def __init__(self, text, link, lok):
+    self.text = text
+    self.link = link
+    self.lok = lok
+
+
+def click(self):
+    return "Клик по локатору - {}" .format(self.lok)
+
+#Создаем экземпляры класса
+
+home_two = Button_Two ('Домой', '/home', 'button#home')
+
+# Вызываем метод
+
+print(home_two.click())
