41f50dcc283ab82fd583f795d1f9a13add56a18a
Irena_Markova <m.irene@mail.ru>
2023-10-14 05:37:50 +0300

    three commit

--- python_testing/task_9_oop2.py
+++ python_testing/task_9_oop2.py
@@ -1,9 +1,23 @@
 class Page:
     def __init__(self, url):
         self.url = url
+
     def get(self):
+
         print(self.url)
 
 
 home = Page('https//demoga.com/')
 home.get()
+
+
+class Soda:
+    def __init__(self, ing=None):
+        self.ing = ing
+
+    def show_my_drink (self):
+        if self ing
+        print('Газировка и {добавка}')
+        else:
+        print('Обычная газировка')
+
