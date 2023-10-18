3133d18f4d9e09d1b5756120aafde8a382778abd
Irena_Markova <m.irene@mail.ru>
2023-10-18 19:53:07 +0300

    three commit

--- python_testing/task__oop1.py
+++ python_testing/task__oop1.py
@@ -1,26 +1,29 @@
-class Input:
+from task_9_checks import Checks
+
+
+class Input(Checks):
 
     def __init__(self, loc, text):
-        self.loc = loc
+        super().__init__(loc)
         self.text = text
 
 
-class Button:
+class Button(Checks):
 
     def __init__(self, loc, text):
-        self.loc = loc
+        super().__init__(loc)
         self.text = text
 
 
-class Title:
+class Title(Checks):
     def __init__(self, loc, text):
-        self.loc = loc
+        super().__init__(loc)
         self.text = text
 
 
-class Link:
+class Link(Checks):
     def __init__(self, loc, text):
-        self.loc = loc
+        super().__init__(loc)
         self.text = text
 
 
