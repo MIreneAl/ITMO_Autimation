3cc11c263a0d87b57efff3102003a550b901a707
Irena_Markova <m.irene@mail.ru>
2023-10-28 19:22:43 +0300

    three commit

---
+++ tests/test_2_open_site.py
@@ -0,0 +1,17 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://demoqa.com/")
+time.sleep(5)
+
+# поиск элемента
+
+icon = driver.find_element(By.CSS_SELECTOR, 'header > a > img')
+
+if icon is None:
+    print('Элемент не найден')
+else:
+    print('Элемент найден')
